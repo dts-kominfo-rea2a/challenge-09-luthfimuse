f533f5def9f7c160775fd9eb2bf30ef82c49a434
Luthfi Burhanuddin <luthfi.burhanuddin@gmail.com>
2022-06-20 15:36:58 +0800

    challenge 09 commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,13 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <></>
+        <>
+            <div>
+                <h1 style={{ color:'#008080' }}>Call a Friend</h1>
+                <p style={{ margin:0 }}>Your Friendly Contact App</p>
+                <span style={{ color:'#C0392B', fontSize:'25px' }}> -------------------------------------------------------------</span>
+            </div>
+        </>
     )
 }
 
